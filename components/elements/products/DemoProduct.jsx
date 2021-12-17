5a9b3c278aeb9708c9fe820e0d67f9568dc8388c
Wahab Khan <kwahab789@gmail.com>
2021-12-17 16:03:26 +0500

    demo api

--- components/elements/products/DemoProduct.jsx
+++ components/elements/products/DemoProduct.jsx
@@ -6,15 +6,20 @@ const DemoProduct = ({ image, product, title, price }) => {
     return (
         <>
             <div className="row">
-                <div className="ps-product">
-                    <div className="ps-product__thumbnail">
+                <div className="ps-product px-24">
+                    <div
+                        className="ps-product__thumbnail"
+                        style={{
+                            margin: 'auto',
+                            width: '200px',
+                        }}>
                         <img src={image} alt="asd" />
                         <ModuleProductActions product={product} />
                     </div>
                     <div className="ps-product__container">
                         <a className="ps-product__vendor">{title}</a>
                         <div className="ps-product__content">
-                            {title}
+                            {/* {title} */}
                             <div className="ps-product__rating">
                                 <Rating />
                                 <span>02</span>
