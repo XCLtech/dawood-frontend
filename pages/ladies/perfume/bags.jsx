9e3d24e7681b28d11bb9e7445bb68a60c4c8d05b
Wahab Khan <kwahab789@gmail.com>
2021-12-22 12:14:25 +0500

    changes

--- pages/ladies/perfume/bags.jsx
+++ pages/ladies/perfume/bags.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import WidgetShopCategories from '~/components/shared/widgets/WidgetShopCategories';
+import PageContainer from '~/components/layouts/PageContainer';
+import Newletters from '~/components/partials/commons/Newletters';
+import Product from '~/components/elements/products/DemoProduct';
+import data from '@@/public/static/data/demo.json';
 
-const bags = () => {
-    return <div>asd</div>;
+const index = () => {
+    return (
+        <PageContainer title="Shop">
+            <div className="ps-page--shop">
+                <div className="ps-container">
+                    <div className="ps-layout--shop">
+                        <div className="ps-layout__left">
+                            <WidgetShopCategories />
+                            {/* <WidgetShopBrands /> */}
+                            {/* <WidgetShopFilterByPriceRange /> */}
+                        </div>
+                        <div className="ps-layout__right ">
+                            <div className="d-flex justify-content-center row">
+                                {data.riceFlour.map((item, index) => (
+                                    <Product
+                                        key={index}
+                                        image={item.url}
+                                        title={item.title}
+                                        price={item.price}
+                                    />
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Newletters />
+        </PageContainer>
+    );
 };
 
-export default bags;
+export default index;
