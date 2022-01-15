7be85b76cd4f2ddf983e03c17fd254cb84985fc0
Wahab Khan <kwahab789@gmail.com>
2022-01-15 15:30:13 +0500

    mob add to cart

--- components/shared/headers/modules/MobileHeaderActions.jsx
+++ components/shared/headers/modules/MobileHeaderActions.jsx
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Link from 'next/link';
 import AccountQuickLinksMobile from './AccountQuickLinksMobile';
+import { useCart } from 'react-use-cart';
 
 const MobileHeaderActions = ({ auth, ecomerce }) => {
-    const { cartItems } = ecomerce;
+    const { totalUniqueItems } = useCart();
+
+    // const { cartItems } = ecomerce;
     return (
         <div className="navigation__right">
-            <Link href="/account/shopping-cart">
+            <Link href="/khan/shop">
                 <a className="header__extra" href="#">
                     <i className="icon-bag2"></i>
                     <span>
-                        <i>{cartItems ? cartItems.length : 0}</i>
+                        {/* <i>{cartItems ? cartItems.length : 0}</i> */}
+                        <i>{totalUniqueItems}</i>
                     </span>
                 </a>
             </Link>
