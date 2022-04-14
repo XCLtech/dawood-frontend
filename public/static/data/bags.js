ead86dfb10ac5cbb0d427db535d9ee0cdcced12f
wabbi-khan <kwahab789@gmail.com>
2022-04-14 17:17:35 +0500

    ok

---
+++ public/static/data/bags.js
@@ -0,0 +1,68 @@
+const data = {
+    product: [
+        {
+            id: 1,
+            title: 'Hand Purse',
+            url: '/static/uploads/handpurse/product-1.jpg',
+            price: 1999,
+        },
+
+        {
+            id: 2,
+            title: 'Hand Purse',
+            url: '/static/uploads/handpurse/product-2.jpg',
+            price: 1999,
+        },
+        {
+            id: 3,
+            title: 'Hand Purse',
+            url: '/static/uploads/handpurse/product-3.jpg',
+            price: 1999,
+        },
+        {
+            id: 4,
+            title: 'Hand Purse',
+            url: '/static/uploads/handpurse/product-4.jpg',
+            price: 1999,
+        },
+        {
+            id: 5,
+            title: 'Hand Purse',
+            url: '/static/uploads/handpurse/product-5.jpg',
+            price: 2499,
+        },
+        {
+            id: 6,
+            title: 'Hand Purse',
+            url: '/static/uploads/handpurse/product-6.jpg',
+            price: 1999,
+        },
+
+        {
+            id: 7,
+            title: 'Hand Purse',
+            url: '/static/uploads/handpurse/product-7.jpg',
+            price: 2999,
+        },
+        {
+            id: 8,
+            title: 'Hand Purse',
+            url: '/static/uploads/handpurse/product-8.jpg',
+            price: 1999,
+        },
+        {
+            id: 9,
+            title: 'Hand Purse',
+            url: '/static/uploads/handpurse/product-9.jpg',
+            price: 1999,
+        },
+        {
+            id: 10,
+            title: 'Hand Purse',
+            url: '/static/uploads/handpurse/product-10.jpg',
+            price: 2499,
+        },
+    ],
+};
+
+export default data;
