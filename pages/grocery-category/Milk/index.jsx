55f85bf71dfd27d9f71794d68ea8416075cb44f8
wabbi-khan <kwahab789@gmail.com>
2022-04-25 13:01:44 +0500

    testing

--- pages/grocery-category/Milk/index.jsx
+++ pages/grocery-category/Milk/index.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import WidgetShopCategories from '~/components/shared/widgets/WidgetShopCategories';
+// import WidgetShopCategories from '~/components/shared/widgets/WidgetShopCategories';
 import PageContainer from '~/components/layouts/PageContainer';
 import Newletters from '~/components/partials/commons/Newletters';
 import Product from '~/components/elements/products/DemoProduct';
 // import data from '@@/public/static/data/bags';
-import WidgetShopBrands from '~/components/shared/widgets/WidgetShopBrands';
-import WidgetShopFilterByPriceRange from '~/components/shared/widgets/WidgetShopFilterByPriceRange';
+// import WidgetShopBrands from '~/components/shared/widgets/WidgetShopBrands';
+// import WidgetShopFilterByPriceRange from '~/components/shared/widgets/WidgetShopFilterByPriceRange';
 import { CartProvider } from 'react-use-cart';
 import { useEffect } from 'react';
 import Axios from 'axios';
 import { useState } from 'react';
-
+import Spinner from '~/components/spinner/index';
 const index = () => {
     const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+
     useEffect(() => {
         const fetchBags = async () => {
             try {
@@ -20,6 +22,7 @@ const index = () => {
                     `https://dawoodddocker.herokuapp.com/api/v1/product/id/84`
                 );
                 setData(data.data.data);
+                setLoading(true);
             } catch (error) {
                 console.log(error);
             }
@@ -39,20 +42,27 @@ const index = () => {
                                 <WidgetShopFilterByPriceRange />
                             </div> */}
                             {/* <div className="ps-layout__right "> */}
-                                {/* <div className="d-flex justify-content-center row"> */}
-                                <div className="d-flex  row">
-                                     {data.filter((index)=>index.Sub_Cat=="LIQUID MILK").map(item=>(
-                                        <Product
-                                        key={item}
-                                        image={item?.imgUrl}
-                                         title={item.title}
-                                         price={item.price}
-                                         item={item}
-
-                                        />
-                                    ))
-                                }
-                                    {/* {data.map((item, index) => (
+                            {/* <div className="d-flex justify-content-center row"> */}
+                            <div className="d-flex  row">
+                                {loading ? (
+                                    data
+                                        .filter(
+                                            (index) =>
+                                                index.Sub_Cat == 'LIQUID MILK'
+                                        )
+                                        .map((item) => (
+                                            <Product
+                                                key={item}
+                                                image={item?.imgUrl}
+                                                title={item.title}
+                                                price={item.price}
+                                                item={item}
+                                            />
+                                        ))
+                                ) : (
+                                    <Spinner />
+                                )}
+                                {/* {data.map((item, index) => (
                                         <Product
                                             key={index}
                                             image={item?.imgUrl}
@@ -61,7 +71,7 @@ const index = () => {
                                             item={item}
                                         />
                                     ))} */}
-                                </div>
+                            </div>
                             {/* </div> */}
                         </div>
                     </div>
@@ -72,4 +82,4 @@ const index = () => {
     );
 };
 
-export default index
+export default index;
