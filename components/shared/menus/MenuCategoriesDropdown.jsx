9dc0eaeb61287375a334e25080a63250e64cb222
wabbi-khan <kwahab789@gmail.com>
2022-04-30 13:57:02 +0500

    testing

--- components/shared/menus/MenuCategoriesDropdown.jsx
+++ components/shared/menus/MenuCategoriesDropdown.jsx
@@ -6,22 +6,22 @@ const MenuCategoriesDropdown = () => {
     return (
         <div className="menu--product-categories">
             <div className="menu__toggle" style={{ color: 'white' }}>
-                <i className="icon-menu"></i>
+                <i className="icon-menu"> </i>{' '}
                 <span
                     style={{
                         color: 'white',
                         fontSize: '20px',
                         fontWeight: 'bold',
                     }}>
-                    Grocery
-                </span>
-            </div>
-            <div className="menu__content" style={{ color: 'black' }}>
+                    Grocery{' '}
+                </span>{' '}
+            </div>{' '}
+            <div className="menu__content" style={{ color: 'white' }}>
                 <Menu
                     source={menuData.product_categories}
                     className="menu--dropdown"
                 />
-            </div>
+            </div>{' '}
         </div>
     );
 };
