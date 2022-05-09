cf90f858c119af182c34c7d76bdf8781aa188873
wabbi-khan <kwahab789@gmail.com>
2022-05-09 15:11:51 +0500

    test

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,11 +3,11 @@ import SiteFeatures from '~/components/partials/homepage/home-default/SiteFeatur
 import HomeAdsColumns from '~/components/partials/homepage/home-default/HomeAdsColumns';
 import HomeAds from '~/components/partials/homepage/home-default/HomeAds';
 import DownLoadApp from '~/components/partials/commons/DownLoadApp';
-import NewArrivals from '~/components/partials/homepage/home-default/NewArrivals';
+// import NewArrivals from '~/components/partials/homepage/home-default/NewArrivals';
 import Newletters from '~/components/partials/commons/Newletters';
 import HomeDefaultDealOfDay from '~/components/partials/homepage/home-default/HomeDefaultDealOfDay';
 import HomeDefaultTopCategories from '~/components/partials/homepage/home-default/HomeDefaultTopCategories';
-import HomeDefaultProductListing from '~/components/partials/homepage/home-default/HomeDefaultProductListing';
+// import HomeDefaultProductListing from '~/components/partials/homepage/home-default/HomeDefaultProductListing';
 import HomeDefaultBanner from '~/components/partials/homepage/home-default/HomeDefaultBanner';
 import PageContainer from '~/components/layouts/PageContainer';
 import { CartProvider } from 'react-use-cart';
